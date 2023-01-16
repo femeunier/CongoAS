16b2fc43956615b9d6b225792a2da029d174c0c8
femeunier <felicien.meunier@gmail.com>
2023-01-16 12:23:00 +0100

    Edit att

--- R/attributes2CMIP6name.R
+++ R/attributes2CMIP6name.R
@@ -1,18 +1,34 @@
-attributes2CMIP6name <- function(var="cVeg",
-                                 timestep="Lmon",
-                                 model="CanESM5",
-                                 scenario="1pctCO2",
-                                 variant="r2i1p1f1",
-                                 grid="gn",
-                                 init.year=1850,
-                                 init.month=1,
-                                 end.year=2000,
-                                 end.month=12){
+attributes2CMIP6name <- function(cdf = data.frame(var="cVeg",
+                                                  timestep="Lmon",
+                                                  model="CanESM5",
+                                                  scenario="1pctCO2",
+                                                  variant="r2i1p1f1",
+                                                  grid="gn",
+                                                  init.year=1850,
+                                                  init.month=1,
+                                                  end.year=2000,
+                                                  end.month=12)){
 
-  CMIP6name <- paste0(paste(var,timestep,model,
-                            scenario,variant,grid,
-                            paste0(as.character(init.year),sprintf("%02d",init.month),"-",s.character(end.year),sprintf("%02d",end.month)),
-                            sep = "_"),".nc")
+  var=cdf[["var"]]
+  timestep=cdf[["timestep"]]
+  model=cdf[["model"]]
+  scenario=cdf[["scenario"]]
+  variant=cdf[["variant"]]
+  grid=cdf[["grid"]]
+  init.year=cdf[["init.year"]]
+  init.month=cdf[["init.month"]]
+  end.year=cdf[["end.year"]]
+  end.month=cdf[["end.month"]]
+
+  CMIP6name <- c()
+  for (i in (1:length(var))){
+    CMIP6name[i] <- paste0(paste(var[i],timestep[i],model[i],
+                              scenario[i],variant[i],grid[i],
+                              paste0(as.character(init.year[i]),sprintf("%02d",init.month[i]),"-",s.character(end.year[i]),sprintf("%02d",end.month[i])),
+                              sep = "_"),".nc")
+  }
+
+
 
 
   return(CMIP6name)
